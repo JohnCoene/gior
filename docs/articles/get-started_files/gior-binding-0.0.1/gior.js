6439e6a1aa0756e0d01a3173c197385fc8040e4e
JohnCoene <jcoenep@gmail.com>
2018-10-03 12:02:45 +0200

    site improv

---
+++ docs/articles/get-started_files/gior-binding-0.0.1/gior.js
@@ -0,0 +1,96 @@
+HTMLWidgets.widget({
+
+  name: 'gior',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    var container,
+        controller;
+
+    return {
+
+      renderValue: function(x) {
+
+        container = document.getElementById( el.id );
+        controller = new GIO.Controller( container );
+
+        controller.addData( x.data );
+
+        if(x.hasOwnProperty('initCountry')){
+          controller.setInitCountry( x.initCountry );
+        }
+        controller.configure(x.configs);
+
+        function share ( selectedCountry, relatedCountries ) {
+          Shiny.setInputValue(el.id + '_selected' + ":giorParseJS", selectedCountry);
+          Shiny.setInputValue(el.id + '_related' + ":giorParseJS", relatedCountries);
+        }
+
+        if (HTMLWidgets.shinyMode) {
+          controller.onCountryPicked( share );
+        }
+
+        if(x.hasOwnProperty('style')){
+          controller.setStyle( x.style );
+        }
+
+        controller.init();
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      },
+
+      getMap: function(){
+        return( controller );
+      }
+
+    };
+  }
+});
+
+function get_gior(id){
+
+  var htmlWidgetsObj = HTMLWidgets.find("#" + id);
+
+  var gior;
+
+  if (typeof htmlWidgetsObj != 'undefined') {
+    gior = htmlWidgetsObj.getMap();
+  }
+
+  return(gior);
+}
+
+if (HTMLWidgets.shinyMode) {
+
+  Shiny.addCustomMessageHandler('g_clear_p',
+    function(data) {
+      var globe = get_gior(data.id);
+      if (typeof globe != 'undefined') {
+        globe.clearData();
+      }
+  });
+
+  Shiny.addCustomMessageHandler('g_data_p',
+    function(data) {
+      var globe = get_gior(data.id);
+      if (typeof globe != 'undefined') {
+        globe.addData(data.json);
+      }
+  });
+
+  Shiny.addCustomMessageHandler('g_init_p',
+    function(data) {
+      var globe = get_gior(data.id);
+      if (typeof globe != 'undefined') {
+        globe.init();
+      }
+  });
+
+}
